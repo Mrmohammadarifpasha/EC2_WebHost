060d104616397c0775bda591afd1eec93ed43f99
Mrmohammadarifpasha <mrmohammadarifpasha@gmail.com>
2025-06-26 14:56:06 +0530

    Update web_script.sh

--- web_script.sh
+++ web_script.sh
@@ -6,12 +6,12 @@ sudo apt upgrade -y
 sudo apt install apache2 -y
 sudo apt update -y
 
-sudo systemctl start apache2 -y
-sudo systemctl enable apache2 -y
+sudo systemctl start apache2
+sudo systemctl enable apache2
 
 sudo mv index.html /var/www/html
 sudo mv CONTACT.html /var/www/html
 sudo mv style.css /var/www/html
 sudo mv Arif.jpg /var/www/html
 
-sudo systemctl restart apache2 -y
+sudo systemctl restart apache2
